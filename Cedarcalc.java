8728d3263e29da4efc9967744a119f5682c046f9
Anish092 <37304018+Anish092@users.noreply.github.com>
2018-09-12 10:13:56 -0400

    first

---
+++ Cedarcalc.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Cedarcalc{
+
+	public static void main(String[] args) {
+
+	Scanner keyboard = new Scanner(System.in);
+	int people, chaperones, admission, allowance;
+	double admission2, totad, bus, bus2, bus3, total;
+
+
+	System.out.println("How many kids will be attending: ");
+	people = keyboard.nextInt();
+	chaperones = people/4;
+	admission = chaperones + people;
+
+	System.out.println("When will this trip occur(type 1 for spring and 2 for fall): ");
+	admission2 = keyboard.nextInt();
+	if (admission2 == 1){
+	    admission2 = 39.00;
+		   }
+    if (admission2 == 2){
+		admission2 = 42.00;
+		   }
+     totad = admission * admission2;
+
+    System.out.println("What bus will you take(type 1 for school and 2 for charter): ");
+		bus = keyboard.nextInt();
+
+    allowance = admission * 25;
+
+    System.out.printf("%50s", "You need this many chaperones: ");
+    System.out.printf("%24s\n", chaperones);
+	System.out.printf("%50s", "Cost of admission: ");
+    System.out.printf("%24s\n", totad);
+	System.out.printf("%50s", "Cost of allowance: ");
+    System.out.printf("%24s\n", allowance);
+	if (bus == 1){
+			    bus = 1300.42;
+			    bus2 = admission/44 + 1;
+			    bus3 = (bus * bus2);
+			    System.out.printf("%50s", "Cost of buses: ");
+                System.out.printf("%24s\n", bus3);
+			    total = chaperones + totad + bus3 + allowance;
+			    System.out.printf("%50s", "Total: ");
+                System.out.printf("%24s\n", total);
+				   }
+    if (bus == 2){
+				bus = 1738.92;
+				bus2 = admission/54 + 1;
+				bus3 = (bus * bus2);
+				System.out.printf("%50s", "Cost of buses: ");
+				System.out.printf("%24s\n", bus3);
+				total = chaperones + totad + bus3 + allowance;
+			    System.out.printf("%50s", "Total: ");
+                System.out.printf("%24s\n", total);
+		           }
+
+
+
+
+
+
+
+
+      }
+}
